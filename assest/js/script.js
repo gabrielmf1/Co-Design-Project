4cc177e53d941150d7d08ce2c00463f0bde113ac
Gabriel Miras <gabriel.miras.f@gmail.com>
2018-09-25 19:32:47 -0300

    AT

---
+++ assest/js/script.js
@@ -0,0 +1,43 @@
+/*
+  Se não sabe o que é isto, volte para os vídeos de JavaScript.
+*/
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  /*
+    Se não sabe o que é isto, volte ao ingrediente 2.
+  */
+  let db = connect('https://p-co-desig.firebaseio.com/')
+
+  /*
+    Uma query string é uma maneira de colocar um dicionário
+    no próprio endereço da página. O início dela é marcado
+    por um "?" e ela consiste em um número indeterminado de
+    pares "chave=valor" separados por "&".
+
+    A função extract lê a query string da página atual e
+    transforma ela em um dicionário JavaScript usual.
+  */
+  let params = extract()
+
+  /*
+    Se não sabe o que é isto, volte ao ingrediente 3.
+  */
+
+/*  let path = "/area/" + params["projeto"]*/
+  let path = '/' + params['area'] + '/' + params['projeto']
+  console.log(path)
+  db.download(path, function(data) {
+    /*
+      Se não sabe o que é isto, volte ao ingrediente 3.
+    */
+    replace('body', {
+        'D': data['descricao'],
+        'G': data['goal'],
+        'T': data['titulo'],
+        'S': data['semestre'],
+        'M': data['materia'],
+        'I': data['image'],
+    })
+  })
+})
